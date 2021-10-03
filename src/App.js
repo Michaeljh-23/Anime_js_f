a3049d7fdf59cf3ef5cb5a0cecb819b8242a4aa4
Michael Harfenist <hrharfenist@gmail.com>
2021-10-03 18:05:34 -0400

    getting cool

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ const {Anime, stagger} = ReactAnime
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      control: null,
-      progress: 0,
-      currentTime: 0,
-      duration: 0
-    }
+
   }
 
   render() {
     const { name } = this.props;
-    const {control, progress, currentTime, duration} = this.state;
+
     return (
       <div>
-        <Anime control={control}
-          {...this.setState({control: control})}
-          animeConfig={{autoplay: false, duration: 1500, easing: "easeInOutSine"}}
-          initial={{target: "heyBlue", translateY: 1000}}
-        >
+
         <h1 className="HeyBlue">
           Hey {name}
         </h1>
-      </Anime>
       </div>
     );
   }
