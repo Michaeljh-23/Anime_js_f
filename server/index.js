c23d89fe56f9a948016962de9e4b6c72f9bbabee
Michael Harfenist <hrharfenist@gmail.com>
2021-10-04 20:48:22 -0400

    ready

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ let app = express();
 
 var router = require('./routes.js');
 
-app.use(express.json()); //need to put before the router middleware to make it work
+app.use(express.json());
 
 app.use(express.static(__dirname + "/../dist"));
 
